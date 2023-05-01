7fcddf23c8874d820c6df8b072090b036de17d13
Kelta <kelta_ffxiv@gmail.com>
2023-05-01 19:37:33 +0200

    add start of something

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="bg">
+    <h1>HoH for sephi</h1>
+  <highcharts :options="chartOptions"></highcharts>
+
+
+  </div>
+</template>
+
+<script>
+import {Chart} from 'highcharts-vue'
+export default {
+  name: 'App',
+  components: {
+    highcharts: Chart,
+
+    
+  },
+  data() {
+    return {
+      chartOptions: {
+        series: [{
+          data: [1,2,3] // sample data
+        }]
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+}
+
+.bg {
+  height: 100%;
+  width: 100%;
+
+  padding-top: 60px;
+  background: url('https://cdn.discordapp.com/attachments/1068317015347040388/1077959201571221544/ffxiv_dx11_2023-02-22_14-24-38.png');
+
+      display: block;
+    position: absolute;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import HighchartsVue from 'highcharts-vue'
+
+createApp(App).use(HighchartsVue).mount('#app')
+
